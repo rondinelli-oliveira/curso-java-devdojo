1eeb2851b2979ee05510de44b500c0e5a3c70a9a
rondinelli413 <rondinelli.silva@yandeh.com.br>
2020-10-14 23:20:39 -0300

    Aula 05: Tipos primitivos, convenções de código e comentários.

---
+++ curso-java/src/ImprimindoVariaveis.java
@@ -0,0 +1,10 @@
+
+public class ImprimindoVariaveis {
+
+	public static void main(String[] args) {
+		
+		int idadeDosPaisParaCadastro = 12;
+
+		System.out.println("A idade é: " + idadeDosPaisParaCadastro);
+	}
+}
